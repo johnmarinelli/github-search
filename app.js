fc2cc44b5cf00df079ad27e5adbed6e6ca7249c0
John Marinelli <jonmarinelli@hotmail.com>
2015-01-16 12:29:28 -0700

    style, clear array before search, add about section and contact section

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
 (function() {
   var app = angular.module('githubSearch', []);
 
-  var repos = [];
-
   /* service to call github's api */
   app.factory('Search', ['$http', function($http) {
     return { 
       searchGithub: function(keywords, language, pushed, stars, forks) {
         /* build query string out of params */
         var queryString = 'https://api.github.com/search/repositories?q=';
-
-        pushed = '>2014-12-01';
-        stars = '<=10';
-        forks = '<=3';
-
+        
         queryString += keywords;
         queryString += ('+language:'+language);
         queryString += ('+pushed:'+pushed);
         queryString += ('+stars:'+stars);
         queryString += ('+forks:'+forks);
-        queryString += ('&sort=starts&order=desc');
+        queryString += ('&sort=stars&order=desc');
+
         console.log(queryString);
+
         return $http({
           method: 'get',
           url: queryString
@@ -31,13 +27,26 @@
     }
   }]);
 
-  app.controller('SearchController', ['$scope', 'Search', function($scope, Search) {
+  app.controller('SearchController', ['$scope', '$filter', 'Search', function($scope, $filter, Search) {
+    var twoWeeksAgo = Date.now() - (1000*60*60*24*14);
+
+    $scope.parameters = { 
+      keywords: 'math',
+      language: 'ActionScript',
+      pushed: '>'+$filter('date')(twoWeeksAgo, 'yyyy-MM-dd'),
+      stars: '<=10',
+      forks: '<=3'
+    };
+
     $scope.showOptionalParams = false;
     $scope.toggleOptionalParams = function() {
       $scope.showOptionalParams = !$scope.showOptionalParams;
     };
 
     var handleSearchSuccess = function(data, status) {
+      /* clear array */
+      while(Search.searchGithubResults.length > 0) Search.searchGithubResults.pop();
+
       data.items.forEach(function(repo) {
         /* push repos onto Search services' results object */
         Search.searchGithubResults.push(repo);
@@ -47,13 +56,14 @@
     $scope.doSearch = function(p) {
       /* use Search service to search github */
       Search.searchGithub(p.keywords, p.language, p.pushed, p.stars, p.forks)
-              .success(handleSearchSuccess);
+            .success(handleSearchSuccess);
     }
   }]);
   
   app.controller('SearchResultsController', ['$scope', 'Search', function($scope, Search) {
     $scope.results.repos = Search.searchGithubResults;    
   }]);
+
 })();
 
 (function() {
